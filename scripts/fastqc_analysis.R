461f2a3f7f6e6df21b600f15ca0f4bed079ed196
Kaspersen <vi1511@vetinst.no>
2019-04-04 12:56:42 +0200

    added functionality for no grouping

--- scripts/fastqc_analysis.R
+++ scripts/fastqc_analysis.R
@@ -3,6 +3,7 @@ args <- commandArgs(trailingOnly = TRUE)
 
 report_loc <- args[1]
 output_dir <- args[2]
+group_var <- args[3]
 
 # Libraries
 
@@ -81,7 +82,7 @@ prepare_seq_len_data <- function(list) {
 }
 
 # Function that imports and wrangles fastqc data
-get_fastqc_data <- function(filepath) {
+get_fastqc_data <- function(filepath, grouping = FALSE) {
   folders <- folder_names(filepath)
   
   get_files <- file_names_recursive(filepath)
@@ -117,8 +118,13 @@ get_fastqc_data <- function(filepath) {
                   total_deduplicated_percentage)
   names(df_list) <- list_names
   df_list$basic_statistics <- df_list$basic_statistics %>%
-    spread(Measure,Value) %>%
-    left_join(group_df, by = "ref")
+    spread(Measure,Value)
+  
+  if (grouping == TRUE) {
+    df_list$basic_statistics <- df_list$basic_statistics %>%
+      left_join(group_df, by = "ref")
+  }
+  
   return(df_list)
 }
 
@@ -425,13 +431,249 @@ create_plots <- function(df_list) {
   save_plots(p10, "per_base_mean_sequence_quality", 25, 25)
 }
 
-# Create grouping data frame
-group_df <- create_group_df(report_loc)
+# Function that creates and saves plots from fastqc data
+create_plots_nogroups <- function(df_list) {
+  p1 <- df_list$adapter_content %>%
+    # get data ready for boxplot by gathering the columns into two columns
+    gather(key,
+           value,-c(ref,
+                    Position)) %>%
+    # plotting
+    ggplot(aes(factor(
+      Position,
+      levels = unique(Position),
+      ordered = TRUE
+    ), value, color = key)) +
+    geom_boxplot(outlier.size = 0.5) +
+    labs(
+      x = "Position in Read",
+      y = "Percent (%) Adapter Content",
+      color = NULL,
+      title = "Adapter content"
+    ) +
+    scale_colour_jama() +
+    scale_y_continuous(limits = c(0, 20)) +
+    theme_classic() +
+    theme(
+      axis.text.x = element_blank(),
+      axis.title = element_text(size = 14),
+      axis.ticks.x = element_blank(),
+      strip.text = element_text(size = 14),
+      axis.text.y = element_text(size = 12),
+      legend.text = element_text(size = 12),
+      legend.position = "bottom"
+    )
+  
+  p2 <- df_list$per_base_sequence_content %>%
+    gather(key, value,-c(ref, Base)) %>%
+    ggplot(aes(factor(
+      Base, levels = unique(Base), ordered = TRUE
+    ), value, color = key)) +
+    geom_boxplot(outlier.size = 0.5) +
+    labs(
+      x = "Position in Read",
+      y = "Percent (%)",
+      color = NULL,
+      title = "Per base sequence content"
+    ) +
+    theme_classic() +
+    scale_color_jama() +
+    theme(
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank(),
+      axis.title = element_text(size = 14),
+      strip.text = element_text(size = 14),
+      axis.text.y = element_text(size = 12),
+      legend.text = element_text(size = 12),
+      legend.position = "bottom"
+    )
+  
+  p3 <- df_list$sequence_duplication_levels %>%
+    gather(key, value,-c(ref, `Duplication Level`)) %>%
+    ggplot(aes(
+      factor(
+        `Duplication Level`,
+        levels = unique(`Duplication Level`),
+        ordered = TRUE
+      ),
+      value,
+      fill = key
+    )) +
+    geom_boxplot(outlier.size = 0.5) +
+    scale_fill_manual(values = c("#ef8a62",
+                                 "#67a9cf")) +
+    theme_classic() +
+    labs(x = "Duplication Level",
+         y = "Percent (%) of Sequences",
+         fill = NULL,
+         title = "Sequence duplication levels") +
+    theme(
+      axis.ticks.x = element_blank(),
+      axis.title = element_text(size = 14),
+      strip.text = element_text(size = 14),
+      axis.text.y = element_text(size = 12),
+      legend.text = element_text(size = 12),
+      legend.position = "bottom",
+      axis.text.x = element_text(
+        size = 12,
+        angle = 90,
+        hjust = 1,
+        vjust = 0.4
+      )
+    )
+  
+  # p4 <- df_list %>%
+  #   prepare_seq_len_data() %>%
+  #   left_join(., df_list$basic_statistics[, c("ref", "Sequence length")], by = "ref") %>%
+  #   rename(seqlen = "Sequence length") %>%
+  #   ggplot(aes(factor(Length,
+  #                     ordered = TRUE,
+  #                     levels = unique(Length)),
+  #              Count)) +
+  #   geom_boxplot(outlier.size = 0.5) +
+  #   scale_y_continuous(labels = comma) +
+  #   scale_fill_viridis(discrete = TRUE) +
+  #   labs(x = "Read Size",
+  #        y = "Total sequence length",
+  #        title = "Sequence length per read size") +
+  #   guides(fill = FALSE) +
+  #   theme_classic() +
+  #   theme(axis.text.x = element_text(
+  #     angle = 90,
+  #     hjust = 1,
+  #     vjust = 0.4
+  #   )) +
+  #   facet_wrap(~seqlen, scales = "free")
+  
+  p5 <- df_list$per_sequence_quality_scores %>%
+    ggplot(aes(factor(Quality), Count, fill = factor(Quality))) +
+    geom_boxplot(outlier.size = 0.5) +
+    scale_y_continuous(labels = comma) +
+    scale_x_discrete(breaks = c(0, 5, 10, 15, 20, 25, 30, 35, 40)) +
+    scale_fill_viridis(discrete = TRUE) +
+    labs(x = "Quality",
+         y = "Number of reads",
+         title = "Per sequence quality scores") +
+    guides(fill = FALSE) +
+    theme_classic() +
+    theme(
+      axis.text = element_text(size = 12),
+      axis.title = element_text(size = 14),
+      strip.text = element_text(size = 14)
+    )
+  
+  p6 <- df_list$per_sequence_gc_content %>%
+    ggplot(aes(factor(`GC Content`), Count)) +
+    geom_boxplot(outlier.size = 0.5) +
+    labs(x = "GC content (%)",
+         y = "Number of reads",
+         title = "Per sequence GC content") +
+    scale_y_continuous(labels = comma) +
+    scale_x_discrete(breaks = as.character(seq(
+      from = 0, to = 100, by = 10
+    ))) +
+    theme_classic() +
+    theme(
+      axis.text = element_text(size = 12),
+      axis.title = element_text(size = 14),
+      strip.text = element_text(size = 14)
+    )
+  
+  p7 <- df_list$per_base_n_content %>%
+    ggplot(aes(factor(
+      Base, levels = unique(Base), ordered = TRUE
+    ), `N-Count`)) +
+    geom_boxplot(fill = "#e6e6e6",
+                 outlier.size = 0.5) +
+    labs(x = "Position in read",
+         title = "Per base N content") +
+    guides(fill = FALSE) +
+    theme_classic() +
+    theme(
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank(),
+      axis.title = element_text(size = 14),
+      strip.text = element_text(size = 14),
+      axis.text.y = element_text(size = 12)
+    )
+  
+  # This produces a huge figure - excluded for now
+  # p9 <- df_list$per_tile_sequence_quality %>%
+  #   left_join(., df_list$basic_statistics[, c("ref", "Sequence length")], by = "ref") %>%
+  #   rename(seqlen = "Sequence length") %>%
+  #   ggplot(aes(factor(Base,
+  #                     levels = unique(Base),
+  #                     ordered = TRUE),
+  #              factor(Tile),
+  #              fill = Mean)) +
+  #   geom_tile() +
+  #   scale_fill_viridis(limits = c(-10, 5),
+  #                      breaks = c(-10, -8, -6, -4, -2, 0, 2, 4),
+  #                      guide=guide_colourbar(ticks=T,nbin=50,barheight=.5,label=T,barwidth=10)) +
+  #   labs(y = "Tiles",
+  #        x = "Position in read",
+  #        title = "Per tile sequence quality (mean)",
+  #        fill = NULL) +
+  #   theme_classic() +
+  #   theme(legend.position="bottom",
+  #         legend.justification="center",
+  #         legend.direction="horizontal",
+  #         legend.text=element_text(color="grey20"),
+  #         axis.text.x = element_blank(),
+  #         axis.ticks.x = element_blank(),
+  #         axis.text.y = element_text(size = 6)) +
+  #   facet_wrap(~seqlen, scales = "free")
+  
+  p10 <- df_list$per_base_sequence_quality %>%
+    mutate(Base = factor(Base,
+                         levels = unique(Base),
+                         ordered = TRUE)) %>%
+    mutate(xmax = length(unique(Base)) + 1) %>%
+    ggplot(aes(Base,
+               Mean)) +
+    geom_boxplot(outlier.size = 0.4,
+                 fill = "#7f7f7f") +
+    geom_hline(aes(yintercept = 28),
+               color = "green") +
+    labs(x = "Position in read",
+         y = "Sequence quality",
+         title = "Per base mean sequence quality") +
+    scale_y_continuous(limits = c(0, 42)) +
+    theme_classic() +
+    theme(
+      axis.text.x = element_blank(),
+      axis.ticks.x = element_blank(),
+      axis.title = element_text(size = 14),
+      strip.text = element_text(size = 14),
+      axis.text.y = element_text(size = 12)
+    )
+  
+  save_plots(p1, "adapter_content", 25, 35)
+  save_plots(p2, "per_base_sequence_content", 25, 35)
+  save_plots(p3, "sequence_duplication_levels", 25, 30)
+  save_plots(p5, "per_sequence_quality_scores", 25, 35)
+  save_plots(p6, "per_sequence_gc_content", 25, 30)
+  save_plots(p7, "per_base_n_content", 25, 35)
+  save_plots(p10, "per_base_mean_sequence_quality", 25, 25)
+}
 
-# Check output directory
-check_dir(output_dir)
-output_dir <- paste0(output_dir, "/fastqc_results_", Sys.Date())
 
-# Data and plotting
-df_list <- get_fastqc_data(report_loc)
-create_plots(df_list)
+if (group_var == TRUE) {
+  # Create grouping data frame
+  group_df <- create_group_df(report_loc)
+  
+  # Check output directory
+  check_dir(output_dir)
+  output_dir <- paste0(output_dir, "/fastqc_results_", Sys.Date())
+  
+  # Data and plotting
+  df_list <- get_fastqc_data(report_loc, grouping = TRUE)
+  create_plots(df_list) 
+} else {
+  check_dir(output_dir)
+  output_dir <- paste0(output_dir, "/fastqc_results_", Sys.Date())
+  
+  df_list <- get_fastqc_data(report_loc)
+  create_plots_nogroups(df_list)
+}
+
